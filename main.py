a5980b6401b133378599fe19cf863de742b83004
timDeHof <ttdehof@gmail.com>
2025-03-06 21:13:16 -0500

    feat: restrict the game FPS

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     running = True
     while running: 
         screen.fill((0,0,0))
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
     pygame.quit()
 if __name__ == "__main__":
     main()
